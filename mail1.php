df637c16ad67c8ab734986d39ce88abdc9ffed4d
idiotic-jk <hellojaikrishna@gmail.com>
2024-03-14 18:09:43 +0530

    chore(content):added content portfolio

--- mail1.php
+++ mail1.php
@@ -60,37 +60,37 @@
 // }
 
 // sendEmail("hellojaikrishna@gmail.com","hello","hello")
-function sendEmail($mail_to, $mail_subject, $mail_body){
+// function sendEmail($mail_to, $mail_subject, $mail_body){
 
-    $cURL_key = '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';
-    $mail_from = 'rajesh@drongoai.com';
-    $curl = curl_init();
-    curl_setopt_array($curl, array(
-        CURLOPT_URL => "https://api.sendgrid.com/v3/mail/send",
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_ENCODING => "",
-        CURLOPT_MAXREDIRS => 10,
-        CURLOPT_TIMEOUT => 30,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => "POST",
-        CURLOPT_POSTFIELDS => "{\"personalizations\": [{\"to\": [{\"email\": \"$mail_to\"}]}],\"from\": {\"email\": \"$mail_from\"},\"subject\": \"$mail_subject\",\"content\": [{\"type\": \"text/plain\", \"value\": \"$mail_body\"}]}",
-        CURLOPT_HTTPHEADER => array(
-            "authorization: Bearer $cURL_key",
-            "cache-control: no-cache",
-            "content-type: application/json"
-        ),
-    ));
+//     $cURL_key = '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';
+//     $mail_from = 'rajesh@drongoai.com';
+//     $curl = curl_init();
+//     curl_setopt_array($curl, array(
+//         CURLOPT_URL => "https://api.sendgrid.com/v3/mail/send",
+//         CURLOPT_RETURNTRANSFER => true,
+//         CURLOPT_ENCODING => "",
+//         CURLOPT_MAXREDIRS => 10,
+//         CURLOPT_TIMEOUT => 30,
+//         CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+//         CURLOPT_CUSTOMREQUEST => "POST",
+//         CURLOPT_POSTFIELDS => "{\"personalizations\": [{\"to\": [{\"email\": \"$mail_to\"}]}],\"from\": {\"email\": \"$mail_from\"},\"subject\": \"$mail_subject\",\"content\": [{\"type\": \"text/plain\", \"value\": \"$mail_body\"}]}",
+//         CURLOPT_HTTPHEADER => array(
+//             "authorization: Bearer $cURL_key",
+//             "cache-control: no-cache",
+//             "content-type: application/json"
+//         ),
+//     ));
 
-    $response = curl_exec($curl);
-    $err = curl_error($curl);
-    curl_close($curl);
-    if ($err) {
-        echo "cURL Error #:" . $err;
-    } else {
-        echo $response;
-    }
-}
+//     $response = curl_exec($curl);
+//     $err = curl_error($curl);
+//     curl_close($curl);
+//     if ($err) {
+//         echo "cURL Error #:" . $err;
+//     } else {
+//         echo $response;
+//     }
+// }
 
-sendEmail("hellojaikrishna@gmail.com","Hello World", "Hi, welcome to programming");
+// sendEmail("hellojaikrishna@gmail.com","Hello World", "Hi, welcome to programming");
 
-?>
+// ?>
